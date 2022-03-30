b3c8812203da13a8bd9654080404b9323952a3b7
MironFoerster <miron.foerster@gmx.de>
2022-03-30 21:27:41 +0200

    formality func in profile

--- app/src/main/java/com/example/myapplication/Profile.java
+++ app/src/main/java/com/example/myapplication/Profile.java
@@ -5,28 +5,21 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Profile {
-
     int age;
-    double distance;
-    ArrayList<String> log = new ArrayList<String>(); //
-    String emotion;
-    int respect; //Einordnung in verschiedene Menschentypen "Sie", "Du"
-    String faqTopic; //Zu welchem Thema hat der user eine Frage?
-    String funTopic;
-    String intention; //unterhaltung oder Frage, was kannst du Roboter
-    Date date = new Date();
-    String dates = date.toString();
+    //double distance;
+    //ArrayList<String> log = new ArrayList<String>(); //
+    //String emotion;
+    //String faqTopic; //Zu welchem Thema hat der user eine Frage?
+    //String funTopic;
+    //String intention; //unterhaltung oder Frage, was kannst du Roboter
+    //Date date = new Date();
+    //String dates = date.toString();
 
     public Profile(){
 
     }
 
-    public void updateTyp(){
+    public String formality(){
+        return (this.age < 10) ? "child" : (this.age < 20) ? "teen" : "adult";
     }
-
-    public void updateTopic(){
-    }
-
-
-
 }
