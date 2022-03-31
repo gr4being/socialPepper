0b71434f9f8e9542921525cf8a69d6cf3c5edacc
MironFoerster <miron.foerster@gmx.de>
2022-03-31 20:28:42 +0200

    admin konsole finished

--- python/admin.py
+++ python/admin.py
@@ -2,41 +2,53 @@ import os
 import json
 import pprint
 
-question_fp = "questions.txt"
+question_fp = "questions.json"
 
 print("############ Willkommen zur Admin-Anwendung! #############\n")
 
 action = "f"
 
-with open(question_fp, "r") as file:
-    parsed = json.load(file)
+
 
 while not action == "v":
+    with open(question_fp, "r") as file:
+        parsed = json.load(file)
     print("Datei Aktuell:")
     pprint.pprint(parsed)
     action = input("Frage Hinzufügen?(h) Frage Entfernen?(e) Frage Bearbeiten?(b) Anwendung Verlassen?(v) ")
-    if action == "f":
+    if action == "h":
         q = input("\nFragentext eingeben: ")
         a = input("\nAntworttext eingeben: ")
         avg_c = int(sum([q["count"] for q in parsed["questions"]])/(len(parsed["questions"])))
         parsed["questions"].append({"count": max(avg_c,1), "question": q, "answer": a, "keyweights": []})
 
         print("\nWähle die relevanten Keywords für die Frage (y/n):")
+        nrKeywords = 0
         for kw in parsed["keywords"]:
             if input("\t"+kw+": ") == "y":
-                parsed["questions"][-1]["keyweights"].append(1)
+                parsed["questions"][-1]["keyweights"].append(1.0)
+                nrKeywords += 1
             else:
-                parsed["questions"][-1]["keyweights"].append(0)
+                parsed["questions"][-1]["keyweights"].append(0.0)
+       
                 
         if input("\nNeue Keywords zur Frage hinzufügen? (y/n)") == "y":
             
             k = input("\nKeyword (Enter zum Abschließen): ")
             while not k == "":
                 parsed["keywords"].append(k)
-                parsed["questions"][-1]["keyweights"].append(1)
+                nrKeywords += 1
+                parsed["questions"][-1]["keyweights"].append(1.0)
                 for q in parsed["questions"][:-1]:
-                    q["keyweights"].append(0)
+                    q["keyweights"].append(0.0)
                 k = input("Keyword (Enter zum Abschließen): ")
+
+            for kwNr in range(len(parsed["questions"][-1]["keyweights"])):
+                if(nrKeywords != 0):
+                    parsed["questions"][-1]["keyweights"][kwNr] = parsed["questions"][-1]["keyweights"][kwNr]/nrKeywords
+                    
+        print("\nFrage erfolgreich hinzugefügt!\n\n")
+        
 
     elif action == "e":
         text = input("Füge den Text der zu löschenden Frage ein: ")
@@ -47,7 +59,7 @@ while not action == "v":
         except:
             print("Frage nicht gefunden!")
     
-    elif action == b:
+    elif action == "b":
         text = input("Füge den Text der zu bearbeitenden Frage ein: ")
         try:
             q = [q for q in parsed["questions"] if q["question"] == text][0]
@@ -57,16 +69,16 @@ while not action == "v":
             print("Frage nicht gefunden!")
             
         if q is not None:
-            print("Alter Fragentext: "+q["question"])
-            q = input("\nNeuen Fragentext eingeben (Enter für Beibehalten): ")
-            print("Alter Antworttext: "+q["answer"])
-            a = input("\nNeuen Antworttext eingeben (Enter für Beibehalten): ")
+            newQuestionText = input("\nNeuen Fragentext eingeben (Enter für Beibehalten): ")
+            if(not newQuestionText == ""):
+                parsed["questions"][idx]["question"] = newQuestionText
+            newAnswerText = input("\nNeuen Antworttext eingeben (Enter für Beibehalten): ")
+            if(not newAnswerText == ""):
+                parsed["questions"][idx]["answer"] = newAnswerText
 
-            print("Alte Keyweights: "+q["keyweights"])
-
+            
                 
             
-        with open(question_fp, "w") as file:
-            json.dump(parsed, file)
-
-        print("\nFrage erfolgreich hinzugefügt!\n\n")
+    with open(question_fp, "w") as file:
+        json.dump(parsed, file)
+        
